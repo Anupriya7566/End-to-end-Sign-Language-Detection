f5b43efe22a4eae77a3cfdb4804f736cc7f85008
Anupriya <anupriyasharma326@gmail.com>
2024-03-02 22:23:07 +0530

    commit

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from signLanguage.pipeline.training_pipeline import TrainPipeline
+from signLanguage.logger import logging
+
+
+logging.info("welcome to my custom log")
 
 obj = TrainPipeline()
 obj.run_pipeline()
